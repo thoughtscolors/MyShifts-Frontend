01af4d1b43a75926115ab7e3e427aacd2b943754
Betty Chow <bettychchow@gmail.com>
2018-03-22 16:53:32 -0700

    Rewrite codes for dates for big header and schedule header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const baseURL = 'http://localhost:3000'
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  let nowDate = new Date();
+  let day = `0${nowDate.getDate()}`.slice(-2)
+  let month = `0${nowDate.getMonth()+1}`.slice(-2)
+  let date = `${nowDate.getFullYear()}-${month}-${day}`
   
+  let scheduleHeader = document.querySelector('.schedule-header')
+  scheduleHeader.innerHTML = date
+  
+  axios.get(`${baseURL}/shifts/user-shifts`)
+    .then(result => {
+      console.log('result!!!!', result)
+
+      let currentDate  = document.querySelector('#current-date').innerHTML.slice(-10)
+      console.log('ccccccc', currentDate)
+
+        
+
+
+     
+    })
 })
+

621412081210f4cfd9c830af139b87a5cab51b55
Brady <vanderbannond@gmail.com>
2018-03-23 15:10:35 -0700

    resolved merge conflicts with master, need to update shiftID on divs and test functions, need to separate update request and delete user shift functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const baseURL = 'http://localhost:3000'
 
 document.addEventListener('DOMContentLoaded', () => {
+
   console.log('DOMContentLoaded');
 
   // --------------------------------------------------------
@@ -16,7 +17,44 @@ document.addEventListener('DOMContentLoaded', () => {
   confirmButton.addEventListener('click', confirmRelease)
   // ---------------------------------------------------------
 
-});
+  let nowDate = new Date();
+  let day = `0${nowDate.getDate()}`.slice(-2)
+  let month = `0${nowDate.getMonth()+1}`.slice(-2)
+  let date = `${nowDate.getFullYear()}-${month}-${day}`
+
+  let scheduleHeader = document.querySelector('.schedule-header')
+  scheduleHeader.innerHTML = date
+
+  axios.get(`${baseURL}/shifts/user-shifts`)
+    .then(result => {
+      console.log('result!!!!', result)
+
+      let currentDate  = document.querySelector('#current-date').innerHTML.slice(-10)
+      console.log('ccccccc', currentDate)
+
+      let allShifts = result.data.result
+
+      allShifts.forEach(shift => {
+        console.log('shift id', shift.shift_id)
+        axios.get(`${baseURL}/shifts/${shift.shift_id}`)
+          .then(shiftInfo => {
+            let shiftDate = shiftInfo.data.result.date.slice(0, 10)
+            console.log('shiftInfo ====>', shiftInfo)
+            if( shiftDate === date) {
+              let shiftStart = shiftInfo.data.result.start.slice(0, 5)
+              let shiftEnd = Number(shiftStart.slice(0, 2)) + 2
+              let shiftBox = document.getElementById(`${shiftStart}`)
+              let shiftContent = shiftBox.querySelector('.shift-text')
+              console.log('ssssssss', shiftContent)
+              shiftContent.innerHTML = `Your shift: ${shiftStart}-${shiftEnd}:00`
+              shiftBox.style.backgroundColor = '#F7493B'
+              document.getElementById('take-shift').innerHTML = 'Release!'
+            }
+           }
+          )
+      })
+    })
+})
 
 const employee_id = 1;
 
@@ -50,8 +88,8 @@ const confirmRelease = () => {
 
   shifts.forEach(shift => {
     if (shift.classList.contains('staged')) {
-      const shift_id = shift.id
-      const start = shift.dataset.start
+      const shift_id = shift.dataset.shiftId
+      const start = shift.id
       console.log(shift_id, start);
 
       axios.all([
@@ -64,12 +102,12 @@ const confirmRelease = () => {
 
     }
   })
-
+}
 //   axios.post(
 //     .then(res => {
 //       console.log(res);
 //     })
-}
+
 
 // const confirmRequest = () => {
 //   let shift_id = 3

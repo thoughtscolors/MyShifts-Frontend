d3ec037d46209d7afc72bef488991b4358c954c2
Brady <vanderbannond@gmail.com>
2018-03-25 16:25:25 -0700

    separated add shifts delete shifts and requests functions

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ const stageTakeShift = () => {
   userDiv.classList.toggle("staged")
 }
 
-const confirmRelease = () => {
+const createRequestOnConfirm = () => {
   const date = document.querySelector('.schedule-header').textContent
   const shifts = document.querySelectorAll('.shift')
 
@@ -92,33 +92,33 @@ const confirmRelease = () => {
       const start = shift.id
       console.log(shift_id, start);
 
-      axios.all([
-        axios.delete(`${baseURL}/shifts/user-shifts/${shift_id}`),
-        axios.post(`${baseURL}/shifts/requests`, {employee_id, shift_id, start, date})
-      ])
-         .then(axios.spread((res1, res2) => {
-         console.log(res1, res2)
-      }))
-
+      axios.post(`${baseURL}/shifts/requests`, {employee_id, shift_id, start, date})
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }
   })
 }
-//   axios.post(
-//     .then(res => {
-//       console.log(res);
-//     })
 
+const deleteRequestWhenShiftTaken = (requestId) => {
+  axios.delete(`${baseURL}/shifts/requests/${requestId}`)
+  .then(res => {
+    console.log(res))
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
 
-// const confirmRequest = () => {
-//   let shift_id = 3
-//   axios.post(`${baseURL}/shifts/requests`, {
-//       user_id,
-//       shift_id
-//     })
-//      .then(res2 => {
-//        console.log(res1, res2);
-//      })
-//      .catch(err => {
-//        console.log(err);
-//      })
-// }
+const deleteUserShiftWhenTaken = (shift_id) => {
+  axios.delete(`${baseURL}/shifts/user-shifts/${shift_id}`)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}

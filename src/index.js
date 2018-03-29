c47dee5a67ca3708f07cb314ba1cfbd6385204ae
Betty Chow <bettychchow@gmail.com>
2018-03-28 19:27:27 -0700

    Finish refactoring for frontend codes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   renderPage()
 })
 
+let nowDate = new Date();
+let day = `0${nowDate.getDate()}`.slice(-2)
+let month = `0${nowDate.getMonth()+1}`.slice(-2)
+let date = `${nowDate.getFullYear()}-${month}-${day}`
+
 const renderPage = () => {
 console.log('rendering page');
   let nowDate = new Date();
@@ -14,11 +19,11 @@ console.log('rendering page');
 
   let scheduleHeader = document.querySelector('.schedule-header')
   scheduleHeader.innerHTML = date
-
+console.log('xxxxxx')
   // get all requests with nested employees on load
   axios.get(`${baseURL}/shifts/requests`)
   .then(result => {
-
+console.log('rrrrrrresult', result)
     // filter the requests for the current date
     const ofDayRequest = result.data.result.filter(request => request.date.slice(0, 10) === date)
 
@@ -41,55 +46,44 @@ console.log('rendering page');
                 let shiftContent = userShiftBox.querySelector('.shift-text')
 
                 let releaseButton = document.createElement('button')
-                releaseButton.addEventListener('click', stageRelease)
-                userShiftBox.setAttribute('data-shiftId', shift.id)
-                userShiftBox.setAttribute('data-employeeId', employee.id)
+                releaseButton.addEventListener('click', toggleStage)
+                userShiftBox.setAttribute('data-shiftid', shift.shift_id)
+                userShiftBox.setAttribute('data-employeeid', 1)
+                userShiftBox.setAttribute('data-reqid', 0)
                 userShiftBox.classList.add('current')
-
                 shiftContent.innerHTML = `Your shift: ${startTime}-${endTime}:00`
-                userShiftBox.style.backgroundColor = 'red'
-                release.innerHTML = 'Release Shift'
-                release.addEventListener('click', releaseShift)
-  
-                if(!userShiftBox.querySelector('button')) {
-                  userShiftBox.appendChild(release)
-                }
-                
-                
-                // userShiftBox.style.backgroundColor = '#F7493B'
                 releaseButton.innerHTML = 'Release Shift'
 
-                userShiftBox.appendChild(releaseButton)
-
-                if (userShiftBox.classList.contains('staged')) {
-                  userShiftBox.classList.remove('staged')
+                if(!userShiftBox.querySelector('button')) {
+                  userShiftBox.appendChild(releaseButton)
                 }
-
+                
+                if (userShiftBox.classList.contains('staged-release')) {
+                  userShiftBox.classList.remove('staged-release')
+                }
 
               } else {
                 let requestTime = request.start.slice(0, 5)
                 let requestEndTime = Number(requestTime.slice(0,2)) + 4
                 let requestBox = document.getElementById(`${requestTime}`)
                 let requestContent = requestBox.querySelector('.shift-text')
-
                 let takeButton = document.createElement('button')
-                takeButton.addEventListener('click', stageTakeShift)
-                requestBox.setAttribute('data-reqId', request.id)
-                requestBox.setAttribute('data-shiftId', shift.shift_id)
-                requestBox.setAttribute('data-employeeId', employee.id)
+
+                takeButton.addEventListener('click', toggleStage)
+                requestBox.setAttribute('data-reqid', request.id)
+                requestBox.setAttribute('data-shiftid', request.shift_id)
+                requestBox.setAttribute('data-employeeid', employee.id)
                 requestBox.classList.add('request')
+                requestContent.innerHTML = `${name}: ${requestTime}-${requestEndTime}:00`
+                takeButton.innerHTML = 'Take Shift'
 
-                if (requestBox.classList.contains('staged')) {
-                  requestBox.classList.remove('staged')
-                }
-
-
-                  requestContent.innerHTML = `${name}: ${requestTime}-${requestEndTime}:00`
-                  // requestBox.style.backgroundColor = '#4FC4CF'
-                  takeButton.innerHTML = 'Take Shift'
                 if(!requestBox.querySelector('button')) {
                   requestBox.appendChild(takeButton)
                 }
+
+                if (requestBox.classList.contains('staged-take-shift')) {
+                  requestBox.classList.remove('staged-take-shift')
+                } 
               }
             })
           })
@@ -98,22 +92,85 @@ console.log('rendering page');
   })
 }
 
-const releaseShift = (event) => {
+const releaseShift = (shift_id, request_id) => {
   console.log('oooooooo')
-  console.log(event.target.parentNode.id)
-  let date = `${nowDate.getFullYear()}-${month}-${day}`
-  const start = event.target.parentNode.id
+  //console.log(event.target.parentNode.id)
+  //let date = `${nowDate.getFullYear()}-${month}-${day}`
 
-  axios.post(`${baseURL}/shifts/requests`, {start, date, employee_id: 1, shift_id })
-    .then(result => {
-      const request_id = result.id
       axios.patch(`${baseURL}/shifts/user-shifts/${shift_id}`, {request_id})
+       .then(result => {
+         console.log('result in releaseShift', result)
+       })
 
-    })
-
-  
 }
+
+
   
+let columnRight = document.querySelector('#col-right').children
+
+console.log('righttttt', columnRight)
+
+
+
+const confirm = () => {
+  
+  const allShiftBoxes = document.querySelector('#col-right').children
+
+  for(let i = 0; i < allShiftBoxes.length; i++) {
+    
+    let start = allShiftBoxes[i].id
+      let employee_id = allShiftBoxes[i].dataset.employeeid
+      let shift_id = allShiftBoxes[i].dataset.shiftid
+      let shiftContentBox = allShiftBoxes[i].firstChild.nextSibling
+      let shiftContent = shiftContentBox.innerHTML
+      let shiftPeriod = shiftContent.slice(-10)
+      let shiftButton = allShiftBoxes[i].lastChild
+    if(allShiftBoxes[i].classList.contains('staged-release')) {
+      // let start = allShiftBoxes[i].id
+      // let employee_id = allShiftBoxes[i].dataset.employeeid
+      // let shift_id = allShiftBoxes[i].dataset.shiftid
+      // let shiftContentBox = allShiftBoxes[i].firstChild.nextSibling
+      // let shiftContent = shiftContentBox.innerHTML
+      // let shiftPeriod = shiftContent.slice(-10)
+      // let shiftButton = allShiftBoxes[i].lastChild
+
+      createRequest(employee_id, shift_id, start, date)
+
+      allShiftBoxes[i].classList.remove('staged-release')
+      allShiftBoxes[i].classList.remove('current')
+      allShiftBoxes[i].classList.add('request')
+      shiftContentBox.innerHTML = `Justin: ${shiftPeriod}`
+      shiftButton.innerHTML = 'Take Shift'
+
+      console.log('dddddddddd', shiftContent)
+
+    } else if (allShiftBoxes[i].classList.contains('staged-take-shift')) {
+      //let start = allShiftBoxes[i].id
+      let request_id = allShiftBoxes[i].dataset.reqid
+     // let shift_id = allShiftBoxes[i].dataset.shiftid
+      let new_request_id = 0
+
+      deleteRequestWhenTaken(request_id)
+      addToUserShiftsWhenTaken(shift_id, start, date, new_request_id)
+      
+
+      allShiftBoxes[i].classList.remove('staged-take-shift')
+      allShiftBoxes[i].classList.remove('request')
+      allShiftBoxes[i].classList.add('current')
+      allShiftBoxes[i].dataset.reqid = 0
+      allShiftBoxes[i].dataset.employeeid = 1
+      // const shiftContentBox = allShiftBoxes[i].firstChild.nextSibling
+      // const shiftContent = shiftContentBox.innerHTML
+      // let shiftPeriod = shiftContent.slice(-10)
+      shiftContentBox.innerHTML = `Your Shift: ${shiftPeriod}`
+      shiftButton.innerHTML = 'Release Shift'
+    }
+  }
+
+}
+
+const confirmButton = document.querySelector('#confirm')
+confirmButton.onclick = confirm
   
 
   // JUSTIN
